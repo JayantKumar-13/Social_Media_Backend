e763e9fc9e05e8db93e578aab31033ea7005335f
Jayant Kumar <jayantkumar13ssm@gmail.com>
2025-09-24 19:44:35 +0530

    Added the entities

---
+++ src/main/java/com/jayant/Social_Media_Backend/entities/User.java
@@ -0,0 +1,22 @@
+package com.jayant.Social_Media_Backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+
+@Entity
+@Table(name = "user")
+@Data
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private int image;
+
+}
